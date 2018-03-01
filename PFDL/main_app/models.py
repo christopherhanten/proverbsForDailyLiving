a1ea369179a05835e567e84faca5dcd995481cb8
Chanten <controlsltrapidfire@gmail.comm>
2018-03-01 10:42:33 -0600

    adds ability to see proverbs from db.

--- PFDL/main_app/models.py
+++ PFDL/main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class Proverbs(models.Model):
+    date = models.CharField(max_length=12)
+    proverb = models.CharField(max_length=500)
+
+    def __str__(self):
+	       return self.proverb
+          
 # Create your models here.
--- PFDL/main_app/views.py
+++ PFDL/main_app/views.py
@@ -1,19 +1,8 @@
 # main_app/views.py
+#main_app/views.py
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Proverbs
 
 def index(request):
-    return render(request, 'index.html', {'proverbs': proverbs})
-
-
-class Proverb:
-    def __init__(self, date, proverb):
-        self.date = date
-        self.proverb = proverb
-proverbs = [
-    Proverb ('1-Jan', "The generous man enriches himself by giving, whilst the miser hoards himself poor."),
-	Proverb ('2-Jan', "A slip of the foot you may soon recover, but a slip 	of the tongue you may never get over."),
-	Proverb ('3-Jan', "The greatest wealth is contentment with a little."),
-]
-
-# Create your views here.
+    proverb = Proverbs.objects.all()
+    return render(request, 'index.html', {'proverbs':proverb})
