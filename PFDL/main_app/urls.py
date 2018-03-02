3ab06404fb43d37f574b0a99ac19e2eb0cbf8f03
Chanten <controlsltrapidfire@gmail.comm>
2018-03-02 10:10:30 -0600

    Tries to fix proverbs display.

--- PFDL/main_app/urls.py
+++ PFDL/main_app/urls.py
@@ -5,7 +5,10 @@ from views import post_proverb
 
 
 urlpatterns = [
-    url(r'^$', index),
-    url(r'^([0-9]+)/$', show, name = 'show')
-    url(r'^post_url/$', post_treasure, name="post_proverb")
-]
+    url(r'^$', views.index),
+    url(r'^about/$', about),
+    url(r'^results/$', tenets),
+    url(r'^profile/$', prayerList),
+    url(r'^login/$', login),
+    url(r'^profile/$', profile),
+    url(r'^signup/$', SignupView.as_view(), name='signup'),
