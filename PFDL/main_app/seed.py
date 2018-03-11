d4a886c4922f3cfe63be05cfb152bf5f69eba64d
Chanten <controlsltrapidfire@gmail.comm>
2018-03-11 13:08:46 -0500

    Makes changes to README and seed.py

--- PFDL/main_app/seed.py
+++ PFDL/main_app/seed.py
@@ -4,7 +4,7 @@ from .models import Proverb
 with open('/Users/chanten/Desktop/PFDL/PFDL/main_app/static/content/proverbs.csv') as f:
 	reader = csv.reader(f, delimiter = '')
 	for row in reader:
-		_, created = Proverbs.objects.get_or_create(
+		_, created = Proverb.objects.get_or_create(
 			date=row[0],
 			proverb=row[1],
 		)
