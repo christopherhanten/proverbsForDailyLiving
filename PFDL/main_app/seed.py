4c1cabc425abe7781090798c5fe48f3330075c05
chanten <31356942+christopherhanten@users.noreply.github.com>
2018-03-12 09:16:22 -0500

    Update seed.py

--- PFDL/main_app/seed.py
+++ PFDL/main_app/seed.py
@@ -1,8 +1,6 @@
-import csv
-from .models import Proverb
-
 with open('/Users/chanten/Desktop/PFDL/PFDL/main_app/static/content/proverbs.csv') as f:
-	reader = csv.reader(f, delimiter = '')
+	reader = csv.reader(f, delimiter = ',')
+	next(reader, None)
 	for row in reader:
 		_, created = Proverb.objects.get_or_create(
 			date=row[0],
