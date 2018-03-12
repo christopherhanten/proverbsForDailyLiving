d2492969b909d3cd39220249fa3a408c5fb0554f
Chanten <controlsltrapidfire@gmail.comm>
2018-03-12 15:42:05 -0500

    Updates code to try to get data seeding going

--- PFDL/PFDL/settings.py
+++ PFDL/PFDL/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
     'django.contrib.admin',
     'django.contrib.auth',
+    'django_seed',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
